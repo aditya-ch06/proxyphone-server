18357086d80427698ed80dcfb714c09a8047b066
aditya-ch06 <66877613+aditya-ch06@users.noreply.github.com>
2025-08-21 14:33:17 +0700

    Add files via upload

---
+++ transactions.js
@@ -0,0 +1,34 @@
+// routes/transactions.js
+
+import express from 'express';
+import fetch from 'node-fetch'; 
+import authenticateToken from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+const LIVE_URL = 'https://proxy.penatechno.id/api/crm/customer';
+
+router.get('/transactions', authenticateToken, async (req, res) => {  
+  console.log('req.query', req.query)
+  // Extract query parameters from request
+  const { userId } = req.query;
+  let url = LIVE_URL
+  if(userId) {
+    url = `${url}/${userId}/transactions`
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    const data = await response.json();
+    // console.log('data', data);
+    res.json(data);
+  } catch (error) {
+    console.error('Fetch error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+export default router;
+
